f15c2d355ffb4de64ebac1a3558f658791a7cee2
Luke McDonald <luke1a.mcd@gmail.com>
2022-11-08 12:17:06 +1100

    Added Simpleton

--- contactsLib/contacts.cs
+++ contactsLib/contacts.cs
@@ -1,13 +1,22 @@
 ﻿namespace contactsLib;
-public class contact
+public class Contact
 {
     string? contactID;
     string? name;
     string? phoneNumber;
-    public contact(string contactID, string name, string phoneNumber)
+    public Contact(string contactID, string name, string phoneNumber)
     {
         this.name = name;
         this.contactID = name;
         this.phoneNumber = phoneNumber;
+    }
+}
+public class Contacts {
+    List<Contact>? contactsList;
+}
+public class ContactLink {
+    static Contacts pcs = new Contacts();
+    public static Contacts getContacts() {
+        return pcs;
     }
 }
