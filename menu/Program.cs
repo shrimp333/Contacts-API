ef2534d370b90b08a4cab7e12014cb1758ada7e7
Luke McDonald <luke1a.mcd@gmail.com>
2022-11-29 11:44:06 +1100

    formatting and delete class1 file

--- menu/Program.cs
+++ menu/Program.cs
@@ -1,46 +1,53 @@
 ﻿using System;
-
 namespace ContactApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            while(true){
-        Console.Clear();
-        Console.WriteLine("Please select a menu item (1-5)");
-        Console.WriteLine("-----------------------------");
-        Console.WriteLine("1. Add Record\n2. Delete Record\n3. Print\n4. Exit");
-        string? selection = Console.ReadLine();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Please select a menu item (1-5)");
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("1. Add Record\n2. Delete Record\n3. Print\n4. Exit");
+                string? selection = Console.ReadLine();
 
-        if (selection == "1"){
-            AddRecord();
-                             }
-        if (selection == "2"){
-            DeleteRecord();
-                            }
-        if (selection == "3"){
-            PrintRecord();
-                            }
-        if (selection == "4"){
-             Environment.Exit(0);    
-                            }
+                if (selection == "1")
+                {
+                    AddRecord();
+                }
+                if (selection == "2")
+                {
+                    DeleteRecord();
+                }
+                if (selection == "3")
+                {
+                    PrintRecord();
+                }
+                if (selection == "4")
+                {
+                    Environment.Exit(0);
+                }
             }
         }
 
-        static void AddRecord() {
+        static void AddRecord()
+        {
 
         }
 
-        static void DeleteRecord(){
+        static void DeleteRecord()
+        {
 
         }
 
-        static void PrintRecord(){
+        static void PrintRecord()
+        {
 
         }
 
 
-        }
+    }
 
- }
+}
