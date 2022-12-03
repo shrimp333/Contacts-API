ec233c7aca4888bc963a3227cfac31db9d99d264
Luke McDonald <luke1a.mcd@gmail.com>
2022-12-03 16:36:08 +1100

    wrote unit tests

--- unit_test/ContactTest.cs
+++ unit_test/ContactTest.cs
@@ -4,11 +4,40 @@ using contactsLib;
 namespace unit_test;
 public class ContactTest
 {
+
+    public static ContactLink test = new ContactLink();
     [Fact]
-    public void TestAdd()
+    public void testAdd()
     {
-        Contact testContact = new Contact("1234ABC", "John Smith", "04010101011");
-        ContactLink link = new ContactLink();
-        
+        string johnString = "1:John:0459";
+        string janeString = "2:jane:0460";
+        string billString = "3:bill:0461";
+        string bobString = "4:bob:0462";
+        Contact john = new Contact("1", "John", "0459");
+        test.get().AddContact(john);
+        Contact jane = new Contact("2", "jane", "0460");
+        test.get().AddContact(jane);
+        Contact bill = new Contact("3", "bill", "0461");
+        test.get().AddContact(bill);
+        Contact bob = new Contact("4", "bob", "0462");
+        test.get().AddContact(bob);
+        Assert.Equal(johnString, test.get().ConvertToString()[0]);
+        Assert.Equal(janeString, test.get().ConvertToString()[1]);
+        Assert.Equal(billString, test.get().ConvertToString()[2]);
+        Assert.Equal(bobString, test.get().ConvertToString()[3]);
+        return;
+    }
+
+    [Fact]
+    public void testRemove()
+    {
+        string johnString = "1:John:0459";
+        Contact john = new Contact("1", "John", "0459");
+        test.get().AddContact(john);
+        Assert.Equal(johnString, test.get().ConvertToString()[0]);
+        //test to see if john got added
+        test.get().DeleteContact(0);
+        Assert.Equal(0, test.get().ConvertToString().Count);
+        //test to see if the list is now empty
     }
 }
