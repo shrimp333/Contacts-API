acbba488a940535927a6e61fd81f5ed6e3bf9f42
Luke McDonald <luke1a.mcd@gmail.com>
2022-12-05 20:11:40 +1100

    added delete

--- contactsLib_Database/contactsDatabase.cs
+++ contactsLib_Database/contactsDatabase.cs
@@ -1,38 +1,42 @@
-﻿namespace contactsLibDatabase;
-public class Contact
+﻿using System.Data.SqlClient;
+namespace contactsLibDatabase;
+public class Contacts
 {
-    public string contactID {get; set;}
-    public string name {get; set;}
-    public string phoneNumber {get; set;}
-    public Contact(string contactID, string name, string phoneNumber)
+    public static SqlConnection conn = new SqlConnection(@"Server=db-ben.cx9d0jwmg18x.ap-northeast-1.rds.amazonaws.com;Database=contacts;User Id=admin;Password=Greats12345!!;");
+    public void AddContact(string contact)
     {
-        this.name = name;
-        this.contactID = contactID;
-        this.phoneNumber = phoneNumber;
-    }
-}
-public class Contacts {
-    List<Contact> contactsList = new List<Contact>();
-    public void AddContact(Contact contact) {
-        contactsList.Add(contact);
+        string[] arr = contact.Split(",");
+        conn.Open();
+        SqlCommand cmd = new("INSERT INTO CONTACTS VALUES (@cid, @fname, @pnum)", conn);
+        cmd.Parameters.AddWithValue("@cid", arr[0]);
+        cmd.Parameters.AddWithValue("@fname", arr[1]);
+        cmd.Parameters.AddWithValue("@pnum", arr[2]);
+        cmd.ExecuteNonQuery();
+        conn.Close();
         return;
     }
-    public void DeleteContact(string cid){
-        int index = contactsList.FindIndex(x => x.contactID == cid);
-        contactsList.RemoveAt(index);
+    public void DeleteContact(string cid)
+    {
+        conn.Open();
+        SqlCommand cmd = new("DELETE FROM CONTACTS WHERE CUSTOMERID=@cid;", conn);
+        cmd.Parameters.AddWithValue("@cid", cid);
+        cmd.ExecuteNonQuery();
+        conn.Close();
         return;
     }
-    public List<string> ConvertToString(){
-        List<string> stringList = new List<string>();
-        foreach (Contact cont in contactsList) {
-            stringList.Add($"{cont.contactID}:{cont.name}:{cont.phoneNumber}");
-        }
-        return stringList;
-    }
+    // public List<string> ConvertToString()
+    // {
+    //     List<string> stringList = new List<string>();
+    //     foreach (Contact cont in contactsList)
+    //     {
+    //         stringList.Add($"{cont.contactID}:{cont.name}:{cont.phoneNumber}");
+    //     }
+    //     return stringList;
+    // }
 
-    public string Search(string cid)
-    {
-        int index = contactsList.FindIndex(x => x.contactID == cid);
-        return $"{ConvertToString()[index]} at place {index}";
-    }
+    // public string Search(string cid)
+    // {
+    //     int index = contactsList.FindIndex(x => x.contactID == cid);
+    //     return $"{ConvertToString()[index]} at place {index}";
+    // }
 }
--- webapi_database/Controllers/ApiControllerDatabase.cs
+++ webapi_database/Controllers/ApiControllerDatabase.cs
@@ -8,7 +8,6 @@ public class ApiController : ControllerBase
 {
     public static SqlConnection conn = new SqlConnection(@"Server=db-ben.cx9d0jwmg18x.ap-northeast-1.rds.amazonaws.com;Database=contacts;User Id=admin;Password=Greats12345!!;");
     public static ContactLink link = new ContactLink();
-
     private readonly ILogger<ApiController> _logger;
     public ApiController(ILogger<ApiController> logger)
     {
@@ -18,7 +17,6 @@ public class ApiController : ControllerBase
     [HttpGet("get")]
     public string Get()
     {
-        Connect();
         return "The contacts api";
     }
     [HttpGet("add")]
@@ -27,14 +25,7 @@ public class ApiController : ControllerBase
         //contructor is cid,name,phoneN
         try
         {
-            string[] arr = contactConstructor.Split(",");
-            conn.Open();
-            SqlCommand cmd = new("INSERT INTO CONTACTS VALUES (@cid, @fname, @pnum)", conn);
-            cmd.Parameters.AddWithValue("@cid", arr[0]);
-            cmd.Parameters.AddWithValue("@fname", arr[1]);
-            cmd.Parameters.AddWithValue("@pnum", arr[2]);
-            cmd.ExecuteNonQuery();
-            conn.Close();
+            link.get().AddContact(contactConstructor);
         }
         catch (System.Exception)
         {
@@ -62,23 +53,12 @@ public class ApiController : ControllerBase
         string result = "empty";
         try
         {
-            result = link.get().Search(cid);
+            // result = link.get().Search(cid);
         }
         catch (System.Exception)
         {
             return "Something went wrong";
         }
         return $"Success\n{result}";
-    }
-
-    static void Connect()
-    {
-        using (SqlConnection conn = new SqlConnection(@"Server=db-ben.cx9d0jwmg18x.ap-northeast-1.rds.amazonaws.com;Database=master;User Id=admin;Password=Greats12345!!;"))
-        {
-            conn.Open();
-            Console.WriteLine("Connection is just opened");
-            System.Threading.Thread.Sleep(10000);
-            conn.Close();
-        }
     }
 }
